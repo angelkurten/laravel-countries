054758b37bc53696a7ecb07abf7bb4f1b4c90560
Angel Kurten <angelkurten@hotmail.com>
2015-10-28 22:57:18 -0500

    Update LaravelCountriesServicesProvider.php

--- src/LaravelCountriesServicesProvider.php
+++ src/LaravelCountriesServicesProvider.php
@@ -17,7 +17,7 @@ class LaravelCountriesServicesProvider extends ServiceProvider
         $this->publishes([__DIR__ . '/../../config/config.php' => config_path('countries.php')]);
 
         $this->publishes([
-            __DIR__.'/../database/seeders/' => database_path('seeders')
+            __DIR__.'/../database/Seeders/' => database_path('seeders')
         ], 'seeders');
 
         $this->publishes([
